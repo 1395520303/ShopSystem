96c20cc5f4b161264b7ad824420c1e67977d9195
1395520303 <1395520303@qq.com>
2022-11-15 17:13:34 +0800

    6

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,9 @@
 </template>
 
 <script>
-import Home from './pages/home.vue'
-import Nav from './components/nav.vue'
-import SideBar from './components/sidebar.vue'
-import Login from './pages/login.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Home,Nav,SideBar,Login
-  }
-}
+  name: "App",
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
